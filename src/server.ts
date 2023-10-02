40ec3aeadd2bb6be59bfe05cf87dbb0a301b5e23
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-02 13:58:30 -0300

    Habilitando o uso de 'json' no express

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { router, } from "./routes";
 const port = 4000;
 const app  = express();
 
+app.use(express.json());
 app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
