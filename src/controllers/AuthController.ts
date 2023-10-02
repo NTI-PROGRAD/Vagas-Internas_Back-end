1256cdac4dbfb72fcd7d495a1dac62374e4d0cb1
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-02 16:55:11 -0300

    Criando rota para autenticacao

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,54 @@
+import { Request, Response, } from "express";
+import type { Administrador, Coordenacao, } from "@prisma/client";
+import { compare, } from "bcrypt";
+import jwt from "jsonwebtoken";
+import { prismaClient, } from "../database/prismaClient";
+
+export class AuthController
+{
+  public login = async (request: Request, response: Response) => {
+    const { login, senha, } = request.body;
+    const jwtSecretKey = process.env.JWT_SECRET_KEY as string;
+
+    const administratorUser = await this.getAdministratorUser(login);
+    const coordinatorUser   = await this.getCoordinatorUser(login);
+
+    if (await this.validAdministratorAuthentication(senha, administratorUser))
+    {
+      const token = jwt.sign({ userId: administratorUser?.id, }, jwtSecretKey, { expiresIn: 600, });
+      return response.json({ token, });
+    }
+
+    if (await this.validCoordinatorAuthentication(senha, coordinatorUser))
+    {
+      const token = jwt.sign({ userId: coordinatorUser?.id, }, jwtSecretKey, { expiresIn: 600, });
+      return response.json({ token, });
+    }
+
+    return response.status(404).json({ message: "Usuário ou senha incorretos!", });
+  };
+
+  private getAdministratorUser = async (login: string): Promise<Administrador | null> => {
+    return await prismaClient.administrador.findFirst({ where: { login, }, });
+  };
+
+  private getCoordinatorUser = async (login: string): Promise<Coordenacao | null> => {
+    return await prismaClient.coordenacao.findFirst({ where: { login, }, });
+  };
+
+  private matchPassword = async (senha: string, encryptedPassword: string): Promise<boolean> => {
+    return await compare(senha, encryptedPassword);
+  };
+
+  private validAdministratorAuthentication = async (senha: string, administratorUser: Administrador | null): Promise<boolean> => {
+    if (administratorUser === null) return false;
+    
+    return await this.matchPassword(senha, administratorUser.senha);
+  };
+
+  private validCoordinatorAuthentication = async (senha: string, coordinatorUser: Coordenacao | null): Promise<boolean> => {
+    if (coordinatorUser === null) return false;
+
+    return await this.matchPassword(senha, coordinatorUser.senha);
+  };
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Router, } from "express";
+import { AuthController, } from "./controllers/AuthController";
 
 const router = Router();
+
+const auth = new AuthController();
+
+router.post("/login", auth.login);
 
 export { router, };
