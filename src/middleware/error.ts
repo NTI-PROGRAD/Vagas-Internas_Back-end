bac4a585a5effefcce59828e719974dc76699bdd
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-02 17:45:25 -0300

    Implementando tratamento de erros na API

---
+++ src/middleware/error.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction, } from "express";
+import { ApiError, } from "../helpers/api-errors";
+
+export const errorMiddleware = (
+  error: Error & Partial<ApiError>,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode ?? 500;
+  const message = error.statusCode ? error.message : "Internal Server Error!";
+  return response.status(statusCode).json({ message, });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import "express-async-errors";
 import express from "express";
 import { router, } from "./routes";
+import { errorMiddleware, } from "./middleware/error";
 
 const port = 4000;
 const app  = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
