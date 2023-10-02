1c4dcdc8c04921899e94832940bcf8faccfc1c90
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-02 13:57:48 -0300

    Separando definicoes de rotas para arquivo 'routes.ts'

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { router, } from "./routes";
 
 const port = 4000;
 const app  = express();
+
+app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
