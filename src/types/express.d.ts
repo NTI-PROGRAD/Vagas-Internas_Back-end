4d5395efbfd27cd960be1cf4e09661b9ed1156af
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-03 13:16:05 -0300

    Excluindo possibilidade de senha estar no objeto 'request.user'

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import { Administrador, } from "@prisma/client";
 declare global {
   namespace Express {
     export interface Request {
-      user: Partial<Administrador>
+      user: Partial<Omit<Administrador, "senha">>
     }
   }
 }
