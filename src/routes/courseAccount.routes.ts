11da50ab633fe1cc60d44133f406a79ad8d9acb9
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-06 09:53:11 -0300

    Aplicando middleware de verificacao de login nas rotas necessarias

--- src/routes/courseAccount.routes.ts
+++ src/routes/courseAccount.routes.ts
@@ -1,12 +1,13 @@
 import { Router, }                  from "express";
 import { CourseAccountController, } from "../controllers/CourseAccountController";
 import { verifyAdministratorUser, } from "../middleware/verifyAdministratorUser";
+import { verifyLoggedUser, }        from "../middleware/verifyLoggedUser";
 
 const courseAccountController = new CourseAccountController();
 const courseAccountsRoutes    = Router();
 
-courseAccountsRoutes.get("/:idCourseAccount",courseAccountController.read);
-courseAccountsRoutes.put("/:idCourseAccount",courseAccountController.update);
+courseAccountsRoutes.get("/:idCourseAccount", verifyLoggedUser, courseAccountController.read);
+courseAccountsRoutes.put("/:idCourseAccount", verifyLoggedUser, courseAccountController.update);
 courseAccountsRoutes.get("/", verifyAdministratorUser, courseAccountController.readAll);
 
 export { courseAccountsRoutes, };
--- src/routes/grantedTime.routes.ts
+++ src/routes/grantedTime.routes.ts
@@ -1,15 +1,16 @@
 import { Router, }                  from "express";
 import { GrantedTimeController, }   from "../controllers/GrantedTimeController";
 import { verifyAdministratorUser, } from "../middleware/verifyAdministratorUser";
+import { verifyLoggedUser, }        from "../middleware/verifyLoggedUser";
 
 const grantedTimeController = new GrantedTimeController();
 const grantedTimeRoutes     = Router();
 
 grantedTimeRoutes.post("/", verifyAdministratorUser, grantedTimeController.create);
-grantedTimeRoutes.get("/:idGrantedTime", grantedTimeController.read);
+grantedTimeRoutes.get("/:idGrantedTime", verifyLoggedUser, grantedTimeController.read);
 grantedTimeRoutes.delete("/:idGrantedTime", verifyAdministratorUser, grantedTimeController.delete);
 grantedTimeRoutes.get("/", verifyAdministratorUser, grantedTimeController.readAll);
 
-grantedTimeRoutes.get("/:idCourseAccount",grantedTimeController.readGrantedTimesByCourseAccount);
+grantedTimeRoutes.get("/:idCourseAccount", verifyLoggedUser, grantedTimeController.readGrantedTimesByCourseAccount);
 
 export { grantedTimeRoutes, };
