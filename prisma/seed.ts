a77934e0dc7348cf994f287990003d5ebfd77a1e
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-06 11:25:26 -0300

    Adicionando mais registros ao fazer 'seed' no banco de dados

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,9 +5,14 @@ const prisma = new PrismaClient();
 
 async function main()
 {
+  await prisma.placesOffer.deleteMany({});
+  await prisma.courseAccountGetGrantedTime.deleteMany({});
+
+  await prisma.grantedTime.deleteMany({});
+  await prisma.courseAccount.deleteMany({});
+  await prisma.course.deleteMany({});
   await prisma.academicPeriod.deleteMany({});
   await prisma.administratorAccount.deleteMany({});
-  await prisma.courseAccount.deleteMany({});
 
   const administratorAccount1 = await prisma.administratorAccount.create({
     data: {
@@ -49,27 +54,121 @@ async function main()
     },
   });
 
+  const course1 = await prisma.course.create({
+    data: {
+      name: "Administração",
+      classesTime: "Integral",
+      campus: "Recife",
+      academicDegree: "Bacharelado",
+    },
+  });
+
+  const course2 = await prisma.course.create({
+    data: {
+      name: "Arqueologia",
+      classesTime: "Integral",
+      campus: "Recife",
+      academicDegree: "Bacharelado",
+    },
+  });
+
+  const course3 = await prisma.course.create({
+    data: {
+      name: "Arquitetura e Urbanismo",
+      classesTime: "Integral",
+      campus: "Recife",
+      academicDegree: "Bacharelado",
+    },
+  });
+
+  const course4 = await prisma.course.create({
+    data: {
+      name: "Artes Visuais",
+      classesTime: "Integral",
+      campus: "Recife",
+      academicDegree: "Bacharelado",
+    },
+  });
+
+  const course5 = await prisma.course.create({
+    data: {
+      name: "Artes Visuais",
+      classesTime: "Integral",
+      campus: "Recife",
+      academicDegree: "Licenciatura",
+    },
+  });
+
   const courseAccount1 = await prisma.courseAccount.create({
     data: {
-      login: "coordenacao_letras",
+      login: "administracao_bacharelado_recife",
       password: await bcrypt.hash("12345", 10),
-      email: "coord_letras@ufpe.br",
+      email: "coord.administracao@ufpe.br",
+
     },
   });
   
   const courseAccount2 = await prisma.courseAccount.create({
     data: {
-      login: "coordenacao_arquitetura_urbanismo",
+      login: "arqueologia_bacharelado_recife",
       password: await bcrypt.hash("12345", 10),
-      email: "coord_arquiteturaurbanismo@ufpe.br",
+      email: "coord.arqueologia@ufpe.br",
+    },
+  });
+  
+  const courseAccount3 = await prisma.courseAccount.create({
+    data: {
+      login: "arquitetura_e_urbanismo_bacharelado_recife",
+      password: await bcrypt.hash("12345", 10),
+      email: "coord.arquiteturaurbanismo@ufpe.br",
     },
   });
 
-  const periodoAcademico = await prisma.academicPeriod.create({
+  const courseAccount4 = await prisma.courseAccount.create({
+    data: {
+      login: "artes_visuais_bacharelado_recife",
+      password: await bcrypt.hash("12345", 10),
+      email: "coord.artesvisuais@ufpe.br",
+    },
+  });
+
+  const courseAccount5 = await prisma.courseAccount.create({
+    data: {
+      login: "artes_visuais_licenciatura_recife",
+      password: await bcrypt.hash("12345", 10),
+      email: "coord.artesvisuais@ufpe.br",
+    },
+  });
+
+  const academicPeriod1 = await prisma.academicPeriod.create({
     data: {
       idAdministratorAccount: administratorAccount1.id,
       label: "2023.2",
+      activePeriod: false,
+      diplomaBearer: true,
+      externalTransfer: false,
+      internalClassTimeTransfer: true,
+      internalCourseTransfer: true,
+    },
+  });
+  
+  const academicPeriod2 = await prisma.academicPeriod.create({
+    data: {
+      idAdministratorAccount: administratorAccount1.id,
+      label: "2024.1",
       activePeriod: true,
+      diplomaBearer: true,
+      externalTransfer: false,
+      internalClassTimeTransfer: true,
+      internalCourseTransfer: true,
+    },
+  });
+
+  const academicPeriod3 = await prisma.academicPeriod.create({
+    data: {
+      idAdministratorAccount: administratorAccount1.id,
+      label: "2024.2",
+      activePeriod: false,
       diplomaBearer: true,
       externalTransfer: false,
       internalClassTimeTransfer: true,
