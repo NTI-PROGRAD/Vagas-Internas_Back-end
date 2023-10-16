53dc0dbe9d24dc4193a219c0ff762db5001aca45
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-16 17:58:53 -0300

    Implementando geracao de arquivo .docx

--- src/controllers/DocumentGeneratorController.ts
+++ src/controllers/DocumentGeneratorController.ts
@@ -1,4 +1,7 @@
 import { Request, Response, } from "express";
+import { prismaClient, } from "../database/prismaClient";
+import { $Enums, } from "@prisma/client";
+import { DocumentGeneratorUtil, DocxTableData, } from "../util/DocumentGeneratorUtil";
 
 export class DocumentGenerator
 {
@@ -9,6 +12,48 @@ export class DocumentGenerator
 
   public async generateDocx(request: Request, response: Response)
   {
-    return response.status(200).send();
+    const entryModality    = request.query["entry-modality"] as $Enums.EntryModality;
+    const academicPeriodId = request.query["academic-period-id"] as string;
+
+    const docxTableData = await DocumentGeneratorTransactions.getDocxTableData(entryModality, academicPeriodId);
+    await DocumentGeneratorUtil.generateDocxDocument(docxTableData);
+
+    return response.status(200).json({ docxTableData, });
+  }
+}
+
+class DocumentGeneratorTransactions
+{
+  public static async getDocxTableData(entryModality: $Enums.EntryModality, idAcademicPeriod: string): Promise<DocxTableData>
+  {
+    return await prismaClient.$transaction(async (tx) => {
+      const placesOffersWithConstraints = await tx.placesOffer.findMany({
+        where: { entryModality, idAcademicPeriod, },
+        select: {
+          course: {
+            select: {
+              name: true,
+              academicDegree: true,
+              campus: true,
+            },
+          },
+          morning: true,
+          morningAfternoon: true,
+          afternoon: true,
+          afternoonNight: true,
+          night: true,
+        },
+      });
+
+      const academicPeriod = await tx.academicPeriod.findFirst({
+        where: { id: idAcademicPeriod, },
+      });
+
+      return {
+        academicPeriod: academicPeriod?.label ?? "",
+        entryModality,
+        placesOffersWithConstraints,
+      };
+    });
   }
 }
---
+++ src/util/DocumentGeneratorUtil.ts
@@ -0,0 +1,524 @@
+import { writeFileSync, } from "fs";
+import { $Enums, } from "@prisma/client";
+
+import {
+  Document,
+  Table,
+  TableRow,
+  TableCell,
+  Paragraph,
+  Packer,
+  WidthType,
+  TextRun,
+  AlignmentType,
+  SectionType,
+} from "docx";
+
+export type DocxTableData = {
+  academicPeriod: string,
+  entryModality: $Enums.EntryModality,
+  placesOffersWithConstraints: Array<{
+    course: {
+      name: string,
+      academicDegree: $Enums.academicDegree,
+      campus: string,
+    },
+    morning: number,
+    morningAfternoon: number,
+    afternoon: number,
+    afternoonNight: number,
+    night: number
+  }>
+}
+
+export class DocumentGeneratorUtil
+{
+  public static async generateDocxDocument(docxTableData: DocxTableData)
+  {
+    const title = new Paragraph({
+      alignment: AlignmentType.CENTER,
+      children: [
+        new TextRun({
+          text: "ANEXO I",
+          bold: true,
+          size: "11pt",
+        }),
+      ],
+    });
+
+    const subtitle1 = new Paragraph({
+      alignment: AlignmentType.CENTER,
+      children: [
+        new TextRun({
+          text: "VAGAS E ÁREAS DE CONHECIMENTO",
+          bold: true,
+          size: "11pt",
+        }),
+      ],
+    });
+
+    const subtitle2 = new Paragraph({
+      alignment: AlignmentType.CENTER,
+      children: [
+        new TextRun({
+          text: DocumentGeneratorUtil.getTranslatedEntryModality(docxTableData.entryModality).toUpperCase(),
+          bold: true,
+          size: "11pt",
+        }),
+      ],
+    });
+
+    const table = new Table({
+      width: {
+        size: 100,
+        type: WidthType.PERCENTAGE,
+      },
+      rows: [
+        new TableRow({
+          tableHeader: true,
+          children: [
+            new TableCell({
+              shading: { fill: "#BBBBBB", },
+              margins: {
+                top: 150,
+                bottom: 150,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "TRANSFERÊNCIA INTERNA DE CURSO",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: { fill: "#BBBBBB", },
+              margins: {
+                top: 150,
+                bottom: 150,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "TURNO",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+              columnSpan: 6,
+            }),
+            new TableCell({
+              shading: { fill: "#BBBBBB", },
+              children: [],
+            }),
+          ],
+        }),
+        new TableRow({
+          tableHeader: true,
+          children: [
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "CURSO (CAMPUS)",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "GRAU",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "M",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "M/T",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "T",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "T/N",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "N",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            new TableCell({
+              shading: {
+                fill: "#BBBBBB",
+              },
+              margins: {
+                top: 75,
+                bottom: 75,
+                left: 75,
+                right: 75,
+              },
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.CENTER,
+                  children: [
+                    new TextRun({
+                      text: "Total do Curso",
+                      bold: true,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          ],
+        }),
+        ...docxTableData.placesOffersWithConstraints.map((placesOffers) => {
+          const placesSum = placesOffers.morning +
+            placesOffers.morningAfternoon +
+            placesOffers.afternoon +
+            placesOffers.afternoonNight +
+            placesOffers.night;
+
+          return new TableRow({
+            children: [
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.LEFT,
+                    children: [
+                      new TextRun({
+                        text: `${placesOffers.course.name} (${placesOffers.course.campus})`,
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.CENTER,
+                    children: [
+                      new TextRun({
+                        text: placesOffers.course.academicDegree,
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.CENTER,
+                    children: [
+                      new TextRun({
+                        text: placesOffers.morning.toString(),
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.CENTER,
+                    children: [
+                      new TextRun({
+                        text: placesOffers.morningAfternoon.toString(),
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.CENTER,
+                    children: [
+                      new TextRun({
+                        text: placesOffers.afternoon.toString(),
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.CENTER,
+                    children: [
+                      new TextRun({
+                        text: placesOffers.afternoonNight.toString(),
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.CENTER,
+                    children: [
+                      new TextRun({
+                        text: placesOffers.night.toString(),
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              new TableCell({
+                margins: {
+                  top: 125,
+                  bottom: 125,
+                  left: 75,
+                  right: 75,
+                },
+                children: [
+                  new Paragraph({
+                    alignment: AlignmentType.RIGHT,
+                    children: [
+                      new TextRun({
+                        text: placesSum.toString(),
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          });
+        }),
+      ],
+    });
+
+    const doc = new Document({
+      sections: [
+        {
+          children: [
+            title,
+            subtitle1,
+            subtitle2,
+            new Paragraph({}),
+          ],
+        },
+        {
+          properties: { type: SectionType.CONTINUOUS, },
+          children: [ table, ],
+        },
+      ],
+    });
+
+    const filename = DocumentGeneratorUtil.generateFilename(docxTableData);
+    const buffer   = await Packer.toBuffer(doc);
+    
+    writeFileSync(`src/documents/${filename}`, buffer);
+  }
+
+  private static generateFilename(docxTableData: DocxTableData)
+  {
+    const date = new Date();
+
+    const day     = date.getDate().toString().padStart(2, "0");
+    const month   = date.getMonth().toString().padStart(2, "0");
+    const year    = date.getFullYear().toString().padStart(4, "0");
+    const hours   = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+
+    const { entryModality, academicPeriod, } = docxTableData;
+
+    const treatedEntryModality = DocumentGeneratorUtil.getTranslatedEntryModality(entryModality)
+      .toLowerCase()
+      .replace(/\s/g,"-")
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    
+    const treatedAcademicPeriod = academicPeriod.replace(".", "-");
+
+    return `anexo_${treatedEntryModality}_${treatedAcademicPeriod}_${day}${month}${year}${hours}${minutes}${seconds}.docx`;
+  }
+
+  private static getTranslatedEntryModality(entryModality: $Enums.EntryModality): string
+  {
+    switch(entryModality)
+    {
+      case "DiplomaBearer"            : return "Portador de Diploma";
+      case "ExternalTransfer"         : return "Transferência Externa";
+      case "InternalClassTimeTransfer": return "Transferência Interna de Turno";
+      case "InternalCourseTransfer"   : return "Transferência Interna de Curso";
+    }
+  }
+}
