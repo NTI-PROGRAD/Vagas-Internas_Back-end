f1a47b192ec392f93d5c0d55e0652ceea5bd9576
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-17 11:17:10 -0300

    Ativando o cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import "express-async-errors";
 import express from "express";
 import { errorMiddleware, } from "./middleware/error";
 import { router, } from "./routes";
+import cors from "cors";
 
 const port = 4000;
 const app  = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
